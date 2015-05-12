b7a906b7472ead733b32039b342cb42f3ac80b21
ffvictory <vasiljev.andrew@gmail.com>
2015-05-12 20:08:27 +0100

    Gulp : Included Browser-Sync  & Nodemon for live reloads.

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,54 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var clean = require('gulp-clean');
 var complexity = require('gulp-complexity');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
+var nodemon = require('gulp-nodemon');
+var reload = browserSync.reload;
 
-
-gulp.task('default',['clean' , 'complexity'],function(){
+gulp.task('default',['clean' , 'complexity' , 'compress','browser-sync'],function(){
 
 })
 
 gulp.task('size' , function(){
     return gulp.src('./public/stylesheets/*.css')
+    .pipe(plumber())
     .pipe(gulp.dest('./dist/'))
     .pipe(size())
+    .pipe(plumber.stop())
 })
 gulp.task('clean',['size'], function(){
     gulp.src('./dist/')
-        .pipe(clean());
+        .pipe(plumber())
+        .pipe(clean())
+        .pipe(plumber.stop())
 })
 gulp.task('complexity', function(){
     return gulp.src('./public/javascripts/project/*.js')
-        .pipe(complexity());
+        .pipe(plumber())
+        .pipe(complexity())
+        .pipe(plumber.stop());
 });
+gulp.task('compress', function() {
+    return gulp.src('./public/javascripts/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+        .pipe(plumber.stop());
+});
+gulp.task('browser-sync', function() {
+    browserSync.init(['./public/**/**.***'],{
+        port : 7000,
+        proxy : "http://localhost:3000"
+
+    });
+});
+gulp.task('nodemon', function(cb){
+    return nodemon({
+        script : 'app.js'
+
+    }).on('start' , function(){
+        cb();
+    })
+})
