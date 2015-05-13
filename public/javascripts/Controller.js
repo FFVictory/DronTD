b0365069a542ccc23db1d97250262adcb017f40f
ffvictory <vasiljev.andrew@gmail.com>
2015-05-13 21:52:49 +0100

    GIT : fix for incorrect controller commit

--- public/javascripts/Controller.js
+++ public/javascripts/Controller.js
@@ -1,3 +1,24 @@
 /**
  * Created by Andrew on 13/05/2015.
  */
+
+var controller = {};
+
+
+controller.start = (function(){
+    "use strict";
+
+    $(document).ready(function(){
+        setupStage.canvasInit();
+        setupStage.ticker();
+
+    });
+
+
+    var tick =  function(event){
+    };
+
+    return {
+        tick : tick
+    };
+})();
