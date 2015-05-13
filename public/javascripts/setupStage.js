4ff7b0c50584a70bd7b69ae20bee07fe713af891
ffvictory <vasiljev.andrew@gmail.com>
2015-05-13 21:48:19 +0100

    a js file to initialize the canvas and the stage for easel.js

--- public/javascripts/setupStage.js
+++ public/javascripts/setupStage.js
@@ -1,3 +1,28 @@
 /**
  * Created by Drew on 13/05/2015.
  */
+var setupStage = (function(){
+    var stage;
+    var canvasContext;
+    var canvas;
+    //initilizes the canvas
+    var canvasInit = function(){
+        alert("canvasInit");
+        canvas = document.getElementById("gameCanvas");
+        stage = new createjs.Stage(canvas);
+        stage.snapToPixelEnabled = true;
+        canvasContext = canvas.getContext('2d');
+    };
+    //dolbaebi
+    var ticker = function(){
+        createjs.Ticker.setFPS(30);
+        createjs.Ticker.useRAF = true;
+        createjs.Ticker.addEventListener("tick" , controller.start.tick);
+
+    };
+
+    return {
+        canvasInit: canvasInit,
+        ticker: ticker
+    };
+})();
