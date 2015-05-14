0d44a41dc1d7448fbeb78537f7a2d6aceb4c1e05
ffvictory <vasiljev.andrew@gmail.com>
2015-05-14 17:01:10 +0100

    loadImage not hardcoded anymore

--- public/javascripts/utils/assetManagment.js
+++ public/javascripts/utils/assetManagment.js
@@ -6,14 +6,17 @@
 var assetManagment = (function(){
 
     (function(){
-        loadImage();
+        loadImage("images/sprite.jpg");
     }());
 
-    function loadImage(){
+    function loadImage(pathToImage){
         var preload =  new createjs.LoadQueue();
         preload.addEventListener("fileload" , handleFileComplete);
-        preload.loadFile({id : "highGround" , src : "images/sprite.jpg"});
+        preload.loadFile({id : "highGround" , src : pathToImage});
     }
+
+
+
 
     function handleFileComplete(event){
         document.body.appendChild(event.result);
