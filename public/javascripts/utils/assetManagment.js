972dd0add6259c05d82e955a838c48b7d931da44
ffvictory <vasiljev.andrew@gmail.com>
2015-05-17 00:17:56 +0100

    Correct tile loading

--- public/javascripts/utils/assetManagment.js
+++ public/javascripts/utils/assetManagment.js
@@ -2,36 +2,58 @@
  * Created by Drew on 14/05/2015.
  */
 var assetManagement = {};
-
+"use strict";
 assetManagement.start = (function(){
+var stage;
+var preload;
+var level;
+var highGround;
+var images;
 
-    var load = function(){
-        loadImage("images/115.png" , "highground");
-        var highGroundSheet = createHighGroundSheet();
+
+    var load = function(stageInit,levelInit){
+        stage = stageInit;
+        level = levelInit;
+        images = Object.create(null);
+        loadImages();
 
     };
 
-    function loadImage(pathToImage , id){
-        var preload =  new createjs.LoadQueue();
-        preload.addEventListener("fileload" , handleFileComplete);
-        preload.loadFile({id : id , src : pathToImage});
+    var loadImages = function loadImages(){
+        preload =  new createjs.LoadQueue();
+        var manifest = [
+            {src : 'images/115.png' , id : 'highGround'}
+        ];
+        preload.addEventListener("fileload" , handleFileLoad);
+        preload.addEventListener("complete" , loadTiles);
+        preload.loadManifest(manifest);
+
+    };
+
+
+    //Might need to change the scope of stage and level
+    function loadTiles(){
+        for(var i = 0 ; i<level.length ; i++){
+            for(var j =0 ; j<level[i].length ; j++){
+                // There needs to be a decision which tile to load
+
+                var x = (32 * i) + 1;
+                var y = (32 * j) + 1;
+                loadTile(x,y);
+            }
+        }
+
+    }
+    function loadTile(x,y){
+        var img  = new createjs.Bitmap(preload.getResult(images["highGround"]));
+        img.x = x;
+        img.y =y;
+        stage.addChild(img);
     }
 
-    /*
-    function createHighGroundSheet(){
-        var spriteSheet = new createjs.SpriteSheet({
-            "images" : [preloadQueue.getResult("highGround")],
-
-        });
-
-        return spriteSheet
-    }
-    */
-
-
-    function handleFileComplete(event){
-        document.body.appendChild(event.result);
-    }
+    var handleFileLoad = function (event){
+        images[event.item.id] = event.item.id;
+    };
 
     return {
         load : load
